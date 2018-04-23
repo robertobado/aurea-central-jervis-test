fcc0d9fe35eb15e80e235014257e3b56f67724ef
Glauber Macedo <glauber.macedo@aurea.com>
2018-04-23 17:26:01 -0300

    Java build agent Improvements

--- docker/build-agents/java-agent/README.md
+++ docker/build-agents/java-agent/README.md
@@ -1,6 +1,6 @@
 This is a generic Build Agent for java applications.
 
 USAGE
-In the same directory of dockerfile, run:
+In the same directory of dockerfile and .mavenrc run:
 chmod 755 build.sh
 ./build.sh
--- docker/build-agents/java-agent/build.sh
+++ docker/build-agents/java-agent/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #Building a image.
-#Please check if Dockerfile is in the same directory when you perform this script
+#Please check if Dockerfile and .mavenrc is in the same directory when you perform this script
 
 docker build -t jervis/java-agent .
